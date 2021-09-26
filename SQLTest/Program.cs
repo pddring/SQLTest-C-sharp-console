ba5fd0e3defe4bcdacf42e642b4c9f584cff4f49
Pete Dring <dringp@fulford.york.sch.uk>
2021-09-26 07:47:20 +0100

    Added sql injection and safe parameterized example

--- SQLTest/Program.cs
+++ SQLTest/Program.cs
@@ -20,39 +20,94 @@ namespace SQLTest
                 Console.WriteLine("SQL Database test. ");
                 while (running)
                 {
-                    Console.WriteLine("Main Menu:");
-                    Console.WriteLine(
-                        "1) Enter SQL\n" +
-                        "2) Load SQL from a file\n" + 
-                        "3) Export database to SQL file\n" + 
-                        "4) Quit");
-                    // prompt for SQL or command
-                    string input = Console.ReadLine();
-                    switch(input)
+                    try
                     {
-                        // enter and run a single SQL query manually into the console
-                        case "1": 
-                            EnterSingleSQLQuery(connection);
-                            break;
-                        
-                        // load and execute queries from an SQL file
-                        case "2":
-                            LoadFromSQLFile(connection);
-                            break;
+                        Console.WriteLine("Main Menu:");
+                        Console.WriteLine(
+                            "1) Enter SQL\n" +
+                            "2) Load SQL from a file\n" +
+                            "3) Export database to SQL file\n" +
+                            "4) Example: Search for user (vulnerable to SQL injection)\n" +
+                            "5) Example: Search for user (safe)\n" +
+                            "Q) Quit");
+                        // prompt for SQL or command
+                        string input = Console.ReadLine();
+                        switch (input)
+                        {
+                            // enter and run a single SQL query manually into the console
+                            case "1":
+                                EnterSingleSQLQuery(connection);
+                                break;
 
-                        // dump database to SQL file
-                        case "3":
-                            ExportToSQLFile(connection);
-                            break;
+                            // load and execute queries from an SQL file
+                            case "2":
+                                LoadFromSQLFile(connection);
+                                break;
 
-                        // quit
-                        case "4":
-                            running = false;
-                            break;
+                            // dump database to SQL file
+                            case "3":
+                                ExportToSQLFile(connection);
+                                break;
+
+                            // query the sample user database (vulnerable to SQL injection)
+                            case "4":
+                                QueryUserUnsafe(connection);
+                                break;
+
+                            // query the sample user database but filter out any SQL injection
+                            case "5":
+                                QueryUserSafe(connection);
+                                break;
+
+                            // quit
+                            case "q":
+                            case "Q":
+                                running = false;
+                                break;
+                        }
+                    } catch (Exception e)
+                    {
+                        Console.WriteLine("Error: " + e.Message);
                     }
                 }
             }
 
+        }
+
+        private static void QueryUserSafe(SqliteConnection connection)
+        {
+            // ask for which user to search for
+            Console.Write("Search for a user: ");
+            string name = Console.ReadLine();
+
+            string sql = "SELECT * FROM People WHERE firstname LIKE @name OR lastname LIKE @name";
+            
+            // create a list of SQL parameters that can be safely filtered before being added into the SQL statement
+            List<SqliteParameter> sqlParams = new List<SqliteParameter>();
+            sqlParams.Add(new SqliteParameter("@name", $"%{name}%"));
+
+            // Display SQL statement and query parameters
+            Console.WriteLine($"Executing query: {sql}");
+            foreach(SqliteParameter p in sqlParams)
+            {
+                Console.WriteLine($"Param: {p.ParameterName}: {p.Value}");
+            }
+
+            DisplaySQLQueryResults(connection, sql, sqlParams);
+            SqliteParameter[] parameters = new SqliteParameter[1];
+            
+        }
+
+        private static void QueryUserUnsafe(SqliteConnection connection)
+        {
+            // ask for which user to search for
+            Console.Write("Search for a user: ");
+            string name = Console.ReadLine();
+
+            // construct SQL statement
+            string sql = $"SELECT * FROM People WHERE firstname LIKE '%{name}%' OR lastname LIKE '%{name}%'";
+            Console.WriteLine($"Executing query: {sql}");
+            DisplaySQLQueryResults(connection, sql);
         }
 
         private static void ExportToSQLFile(SqliteConnection connection)
@@ -61,30 +116,33 @@ namespace SQLTest
         }
 
         public static void LoadFromSQLFile(SqliteConnection connection) {
-            throw new NotImplementedException();
-        }
-        
-        public static void EnterSingleSQLQuery(SqliteConnection connection)
-        {
-
-            // allow the user to enter SQL over multiple lines
-            Console.WriteLine("Type in some SQL and then enter an empty line to execute it");
-            string sql = "";
-            bool finished = false;
-            while(!finished)
+            Console.Write("Enter filename [default: db.sql]: ");
+            string filename = Console.ReadLine();
+            if(filename == "")
             {
-                string line = Console.ReadLine();
-                if(line == "")
-                {
-                    finished = true;
-                }
-                sql += line;
+                filename = "db.sql";
             }
+            Console.WriteLine($"Loading SQL from {filename}");
+            string sql = System.IO.File.ReadAllText(filename);
+            SqliteCommand cmd = connection.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.ExecuteNonQuery();
+            Console.WriteLine($"Executed:\n {sql}");
+        }
 
+        public static void DisplaySQLQueryResults(SqliteConnection connection, string sql, List<SqliteParameter> sqlParams = null)
+        {
             // execute query
             SqliteCommand cmd = connection.CreateCommand();
 
             cmd.CommandText = sql;
+            
+            // add SQL parameters if they exist
+            if(sqlParams != null)
+            {
+                cmd.Parameters.AddRange(sqlParams);
+            }
+
             try
             {
                 // try to get results
@@ -121,6 +179,26 @@ namespace SQLTest
                 // display error message if something has gone wrong
                 Console.WriteLine("Error: " + e.Message);
             }
+        }
+
+        public static void EnterSingleSQLQuery(SqliteConnection connection)
+        {
+
+            // allow the user to enter SQL over multiple lines
+            Console.WriteLine("Type in some SQL and then enter an empty line to execute it");
+            string sql = "";
+            bool finished = false;
+            while(!finished)
+            {
+                string line = Console.ReadLine();
+                if(line == "")
+                {
+                    finished = true;
+                }
+                sql += line;
+            }
+
+            DisplaySQLQueryResults(connection, sql);
         }
     }
 }
---
+++ SQLTest/db.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE People (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	firstname varchar(255),
+	lastname varchar(255)
+);
+
+CREATE TABLE Secrets (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	PersonID INTEGER,
+	secret varchar(255),
+	FOREIGN KEY(PersonID) REFERENCES People(id)
+);
+
+INSERT INTO People (firstname, lastname) VALUES
+('H', 'Potter'),
+('H', 'Grainger'),
+('R', 'Weasley');
